9917fa7c06464f6ae85d69e3f34bb1e037f4162b
MariaKolyda <iis20004@uom.edu.gr>
2023-05-09 19:04:53 +0300

    init

---
+++ module1/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+import java.io.File;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.InvalidRemoteException;
+import org.eclipse.jgit.api.errors.TransportException;
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
